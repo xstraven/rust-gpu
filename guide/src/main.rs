e68bf5f05ed6ff4ea567f8c8f3d036713c1ef2f5
David Hinrichs <dvd.hnrchs@gmail.com>
2025-10-28 22:34:30 +0100

    train mnist

--- guide/src/main.rs
+++ guide/src/main.rs
@@ -1,14 +1,23 @@
 #![recursion_limit = "256"]
+mod data;
 mod model;
+mod training;
 
 use crate::model::ModelConfig;
+use crate::training::TrainingConfig;
 use burn::backend::Wgpu;
+
+use burn::{backend::Autodiff, optim::AdamConfig};
 
 fn main() {
     type MyBackend = Wgpu<f32, i32>;
+    type MyAutodiffBackend = Autodiff<MyBackend>;
 
-    let device = Default::default();
-    let model = ModelConfig::new(10, 512).init::<MyBackend>(&device);
-
-    println!("{model}");
+    let device = burn::backend::wgpu::WgpuDevice::default();
+    let artifact_dir = "/tmp/guide";
+    crate::training::train::<MyAutodiffBackend>(
+        artifact_dir,
+        TrainingConfig::new(ModelConfig::new(10, 512), AdamConfig::new()),
+        device.clone(),
+    );
 }

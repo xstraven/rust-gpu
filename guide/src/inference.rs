177bac7be65e354451004e2144be9e454e3074ce
David Hinrichs <dvd.hnrchs@gmail.com>
2025-10-29 20:50:24 +0100

    run inference on trained mnist model

---
+++ guide/src/inference.rs
@@ -0,0 +1,25 @@
+use crate::{data::MnistBatcher, training::TrainingConfig};
+
+use burn::{
+    data::{dataloader::batcher::Batcher, dataset::vision::MnistItem},
+    prelude::*,
+    record::{CompactRecorder, Recorder},
+};
+
+pub fn infer<B: Backend>(artifact_dir: &str, device: B::Device, item: MnistItem) {
+    let config = TrainingConfig::load(format!("{artifact_dir}/config.json"))
+        .expect("Config should exist for the model; run train first");
+    let record = CompactRecorder::new()
+        .load(format!("{artifact_dir}/model").into(), &device)
+        .expect("Trained model should exist; run train first");
+
+    let model = config.model.init::<B>(&device).load_record(record);
+
+    let label = item.label;
+    let batcher = MnistBatcher::default();
+    let batch = batcher.batch(vec![item], &device);
+    let output = model.forward(batch.images);
+    let predicted = output.argmax(1).flatten::<1>(0, 1).into_scalar();
+
+    println!("Predicted {predicted} Expected {label}");
+}
--- guide/src/main.rs
+++ guide/src/main.rs
@@ -1,11 +1,13 @@
 #![recursion_limit = "256"]
 mod data;
+mod inference;
 mod model;
 mod training;
 
 use crate::model::ModelConfig;
 use crate::training::TrainingConfig;
 use burn::backend::Wgpu;
+use burn::data::dataset::Dataset;
 
 use burn::{backend::Autodiff, optim::AdamConfig};
 
@@ -19,5 +21,12 @@ fn main() {
         artifact_dir,
         TrainingConfig::new(ModelConfig::new(10, 512), AdamConfig::new()),
         device.clone(),
+    );
+    crate::inference::infer::<MyBackend>(
+        artifact_dir,
+        device,
+        burn::data::dataset::vision::MnistDataset::test()
+            .get(42)
+            .unwrap(),
     );
 }

c6df32e41dcd2a1114b9db959adc6a0091fc9979
David Hinrichs <dvd.hnrchs@gmail.com>
2025-10-29 19:31:12 +0100

    bump to burn 0.19

--- guide/src/training.rs
+++ guide/src/training.rs
@@ -44,7 +44,7 @@ impl<B: Backend> ValidStep<MnistBatch<B>, ClassificationOutput<B>> for Model<B> 
     }
 }
 
-#[derive(Config)]
+#[derive(Config, Debug)]
 pub struct TrainingConfig {
     pub model: ModelConfig,
     pub optimizer: AdamConfig,
@@ -72,7 +72,7 @@ pub fn train<B: AutodiffBackend>(artifact_dir: &str, config: TrainingConfig, dev
         .save(format!("{artifact_dir}/config.json"))
         .expect("Config should be saved successfully");
 
-    B::seed(config.seed);
+    B::seed(&device, config.seed);
 
     let batcher = MnistBatcher::default();
 
@@ -105,6 +105,7 @@ pub fn train<B: AutodiffBackend>(artifact_dir: &str, config: TrainingConfig, dev
     let result = learner.fit(dataloader_train, dataloader_test);
 
     result
+        .model
         .save_file(format!("{artifact_dir}/model"), &CompactRecorder::new())
         .expect("Trained model should be saved successfully");
 }
